df297b18eb374c4a6842395e2ab99f76d380389e
Aung KhantK yaw <aungkk1505@gmail.com>
2026-04-24 10:40:26 +0630

    UPDATES

--- blog-dashboard/src/api/axios.ts
+++ blog-dashboard/src/api/axios.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
+        'x-gateway-secret': import.meta.env.VITE_GATEWAY_SECRET,
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
